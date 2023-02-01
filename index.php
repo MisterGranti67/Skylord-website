e485052b2cbd4a83a95a29d8ae8c164a76b09dce
mrbaguette07 <mathsgovlog@gmail.com>
2023-02-01 16:19:03 +0100

    Footer fini

--- index.php
+++ index.php
@@ -103,43 +103,5 @@
       </div>
     </section>
   </body>
-  <footer id="footer">
-    <div class="footer-menu">
-      <div class="container">
-        <div class="total-haut-footer">
-          <div class="gauche">
-            <a href="https://skylord.fr">
-              <img src="https://skylord.fr/logo.png">
-            </a>
-          </div>
-          <div class="droite">
-            <ul class="menu">
-              <a href="https://skylord.fr/legale/"><li>Nos CGU</li></a>
-              <a href="https://skylord.fr/legale/CGV.php"><li>Nos CGV</li></a>
-              <a href="https://skylord.fr/legale/mentions-legales.php"><li>Mentions légales</li></a>
-              <a href="https://joueur.skylord.fr/vote"><li>Vote et Gagne</li></a>
-              <a href="https://joueur.skylord.fr/shop"><li>Boutique</li></a>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="bas-footer">
-      <div class="container">
-        <div class="total-bas-footer">
-          <div class="gauche">
-            <h1>Skylord</h1>
-            <p>©(Copyright) 2023. Tous les droits réservés</p>
-          </div>
-          <div class="droite">
-            <ul class="menu">
-              <li><a href="https://www.tiktok.com/@play.skylord.fr"><i class="fa-brands fa-tiktok"></i></a></li>
-              <li><a href="https://twitter.com/SkylordFR"><i class="fa-brands fa-twitter"></i></a></li>
-              <li><a href="https://discord.gg/skylord"><i class="fa-brands fa-discord"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
+  <?php include('utils/footer.php'); ?>
 </html>
